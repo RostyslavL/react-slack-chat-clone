aed03c267f3f38998e9da9e3cb06021503b7d17a
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-11-14 15:43:51 +0100

    feat: Set up redux with global state, add user actions, make user data available on global state

--- src/index.js
+++ src/index.js
@@ -5,20 +5,27 @@ import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 import registerServiceWorker from './registerServiceWorker'
 import firebase from './firebase'
-
+import {createStore} from 'redux'
+import {Provider, connect} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import 'semantic-ui-css/semantic.min.css'
-
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   withRouter
 } from 'react-router-dom'
+import {roootReducer} from './reducers/index'
+import {setUser} from './actions/index'
+
+const store = createStore (roootReducer,composeWithDevTools())
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+        //   console.log(user);
+        this.props.setUser(user)
         this.props.history.push('/')
       }
     })
@@ -26,21 +33,24 @@ class Root extends React.Component {
 
   render() {
     return (
-      <Switch>
-        <Route exact path='/' component={App} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-      </Switch>
+        <Switch>
+            <Route exact path='/' component={App} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+        </Switch>
     )
   }
 }
 
-const RootWithAuth = withRouter(Root)
+const RootWithAuth = withRouter(connect(null, {setUser})(Root))
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+    <Provider store={store}>
+        <Router>
+        <RootWithAuth />
+        </Router> 
+    </Provider>
+  ,
   document.getElementById('root')
 )
 registerServiceWorker()
