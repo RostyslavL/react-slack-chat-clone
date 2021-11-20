8c2e86b435e3526f31c1d7dd01b3c60ff4bed32b
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-11-20 22:05:30 +0100

    feat: Add disable button parameter with 'uploading' condition, dynamic dysplay channel name & channel users count

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -170,6 +170,7 @@ class MessageForm extends React.Component {
             content='Upload Media'
             labelPosition='right'
             icon='cloud upload'
+            disabled={uploadState === 'uploading'} 
           />
           </Button.Group>
           <FileModal
