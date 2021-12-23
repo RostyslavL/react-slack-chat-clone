b6639b64212157e72526f2733975ced6df574e70
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-12-23 12:51:05 +0100

    feat:add chosen colors to user data into firebase realtime database

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import { Sidebar, Menu, Divider, Button, Input, Modal, Label ,Icon } from 'semantic-ui-react'
+import { Sidebar, Menu, Divider, Button, Input, Modal, Label ,Icon, Segment } from 'semantic-ui-react'
 import {SliderPicker, TwitterPicker , CompactPicker, CirclePicker } from 'react-color'
+import firebase from '../../firebase'
 
 export default class ColorPanel extends Component {
 
     state = {
         modal:false,
+        primary:'',
+        secondary:'',
+        usersRef: firebase.database().ref('users'),
+        user:this.props.currentUser
     }
     
     openModal = () => {
@@ -16,9 +21,31 @@ export default class ColorPanel extends Component {
         this.setState({modal:false})
     }
 
+    handlePrimaryChange = color => this.setState({primary: color.hex})
+
+    handleSecondaryChange = color => this.setState({secondary: color.hex})
+
+    handleSaveColor = () => {
+        if(this.state.primary && this.state.secondary){
+            this.saveColors(this.state.primary,this.state.secondary)
+        }
+    }
+
+    saveColors = (primary,secondary) => {
+        this.state.usersRef
+        .child(`${this.state.user.uid}/colors`)
+        .push()
+        .update({primary,secondary})
+        .then(() => {
+            console.log('Color are Added');
+            this.closeModal()
+        })
+        .catch(err => console.error(err))
+    }
+
     render() {
 
-        const {modal} = this.state
+        const {modal, primary, secondary} = this.state
 
         return (
             <Sidebar
@@ -45,16 +72,31 @@ export default class ColorPanel extends Component {
                         Choose App Colors
                     </Modal.Header>
                     <Modal.Content>
-                        <Label content="Primary Color"/>
-                            <CirclePicker />
-                        <Label content="Secondary Color"/>
-                            <CirclePicker />
+                        <Segment inverted                        >
+                            <Label content="Primary Color"/>
+                                <CirclePicker 
+                                    color={primary}
+                                    onChange={this.handlePrimaryChange} />
+                        </Segment>
+                        <Segment inverted>
+                            <Label content="Secondary Color"/>
+                                <CirclePicker 
+                                    color={secondary} 
+                                    onChange={this.handleSecondaryChange}/>
+                        </Segment> 
                     </Modal.Content>
                     <Modal.Actions>
-                        <Button color='green' inverted>
+                        <Button 
+                            color='green' 
+                            inverted
+                            onClick={this.handleSaveColor}
+                        >
                             <Icon name='checkmark'/> Save Colors
                         </Button>
-                        <Button color='red' inverted onClick={this.closeModal}>
+                        <Button 
+                            color='red' 
+                            inverted 
+                            onClick={this.closeModal}>
                             <Icon name='remove'/> Cancel
                         </Button>
                     </Modal.Actions>
